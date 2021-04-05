5255f1c7e64d40ddfd13114c28e4d22616e84982
Md-shefat-masum <mshefat924@gmail.com>
2021-04-06 05:22:10 +0600

    vuejs pos invoice maker

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,81 +20,99 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 // components
-Vue.component('home-section', require('./components/homeSection.vue').default);
-Vue.component('product-single-body', require('./components/productSingleBody.vue').default);
-
-
+Vue.component('singleProductBody', require('./components/singleProductBody.vue').default);
+Vue.component('pagination', require('laravel-vue-pagination'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
 if (document.getElementById('app')) {
     const app = new Vue({
         el: "#app",
-        mounted: function () {
-            console.log('mounted');
+        created: function(){
+            this.get_latest_product();
         },
-        created: function () {
-            $.get('/json/latest-products-json', (res) => {
-                console.log(res);
-                this.products = res.data;
-            })
-        },
-        data: function () {
+        data: function(){
             return {
-                modal_view_product: {
-                    name: '',
-                    price: '',
-                    thumb_image: '',
-                },
-                total_cart_product: 0,
-                cart_products: [],
-                cart_total_price: 0,
-                products: [],
+                products: {},
+                pos_poduct_list: [],
             }
         },
         methods: {
-            set_modal_product: function (product) {
-                console.log(product);
-                this.modal_view_product = product;
+            get_latest_product: function(page=1){
+                $.get('/json/latest-products-json/6?page='+page,(res)=>{
+                    this.products = res;
+                })
             },
-            set_cart: function (product) {
-                this.total_cart_product++;
+            search_product: _.debounce(function(key){
+                key.length > 0 ?
+                    $.get('/json/search-product-json/6/'+key,(res)=>{
+                        this.products = res;
+                    })
+                :
+                    this.get_latest_product();
+            },500),
+            add_product_to_pos_list: function(product){
+                let product_check = this.pos_poduct_list.find((item)=>item.id === product.id);
 
-                let product_check = this.cart_products.find((item) => item.id === product.id);
-
-                if (product_check) {
+                if(product_check){
                     product_check.qty++;
-                } else {
-                    let cart_info = {
-                        name: product.name,
-                        id: product.id,
-                        image: product.thumb_image,
-                        price: product.price,
-                        qty: 1,
-                    };
-                    this.cart_products.push(cart_info);
-                }
+                }else{
 
-                this.calculate_total();
-            },
-            remove_cart: function (product) {
-                this.cart_products = this.cart_products.filter((item) => {
-                    if (item.id === product.id) {
-                        this.total_cart_product -= product.qty;
+                    let pos_product = {
+                        id : product.id,
+                        name : product.name,
+                        image : product.thumb_image,
+                        price : product.price,
+                        qty : 1,
                     }
-                    return item.id !== product.id;
-                });
-                this.calculate_total();
+                    this.pos_poduct_list.unshift(pos_product);
+                }
             },
-            calculate_total: function () {
-                this.cart_total_price = this.cart_products.reduce((total, product) => {
-                    return total + (product.price * product.qty);
-                }, 0)
+            remove_post_product: function(product){
+                this.pos_poduct_list = this.pos_poduct_list.filter((item)=>item.id !== product.id);
             },
-
+            update_pos_qty: function(product,qty){
+                let check_product = this.pos_poduct_list.find((item)=>item.id === product.id);
+                check_product.qty = qty;
+            },
         },
+        computed: {
+            get_pos_total_price: function(){
+                return this.pos_poduct_list.reduce((total,product)=>{
+                    return total + (product.price * product.qty)
+                },0);
+            }
+        }
     });
 }
+
+// if(document.getElementById('test')){
+//     const app = new Vue({
+//         el: "#test",
+//         created: function(){
+
+//         },
+//         data: function(){
+//             return{
+//                 a: 10,
+//                 b: 20,
+//                 sum: 0,
+//                 div: 0,
+//                 sub: 0,
+//             }
+//         },
+//         methods: {
+//             calculate: function(){
+//                 this.sum = +(this.a) + +(this.b);
+//                 this.sub = +(this.a) - +(this.b);
+//                 this.div = +(this.a) / +(this.b);
+//             }
+//         }
+
+//     });
+// }
+
---
+++ resources/js/components/singleProductBody.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="product-wrapper bl" >
+        <div class="product-img">
+            <a href="#">
+                <img :src="'/'+product.thumb_image" alt="" class="primary" />
+                <img :src="product.image[0] && '/'+product.image[0].name" alt="" class="secondary" />
+            </a>
+            <div class="product-icon c-fff home3-hover-bg">
+                <ul>
+                    <li>
+                        <a href="#" data-toggle="tooltip" title="" data-original-title="Add to cart"><i class="fa fa-shopping-cart"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" data-toggle="tooltip" title="" data-original-title="Wishlist"><i class="fa fa-heart-o"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" data-toggle="tooltip" title="" data-original-title="Compare"><i class="fa fa-comments"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" data-toggle="tooltip" title="" data-original-title="Accumsan eli"><i class="fa fa-search"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="product-content home3-hover">
+            <h3><a href="#">{{product.name}}</a></h3>
+            <ul>
+                <li><i class="fa fa-star"></i></li>
+                <li><i class="fa fa-star"></i></li>
+                <li><i class="fa fa-star"></i></li>
+                <li><i class="fa fa-star"></i></li>
+                <li><i class="fa fa-star"></i></li>
+            </ul>
+            <span>$ {{product.price}}</span>
+            <button class="btn btn-dark btn-sm mt-4" type="button" @click="add_product_to_pos_list(product)">add to list</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:[
+            'product',
+            'add_product_to_pos_list'
+        ]
+}
+</script>
+
+<style>
+
+</style>
